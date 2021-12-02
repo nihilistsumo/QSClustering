fd22cabdbaa77e7741e68977420a079dfd3f6428
nihilistsumo <sumantakashyapi@gmail.com>
2021-12-02 15:02:34 -0500

    dkm param diff vecs

--- core/clustering.py
+++ core/clustering.py
@@ -97,7 +97,7 @@ class DKM_param(nn.Module):
         x_tr = x.repeat_interleave(y.shape[0], dim=0)
         y_tr = y.repeat((x.shape[0], 1))
         q_tr = q.repeat((y_tr.shape[0], 1))
-        qxy_tr = torch.hstack((x_tr, y_tr, x_tr - y_tr, x_tr - q, y_tr - q))
+        qxy_tr = torch.hstack((x_tr, y_tr, torch.abs(x_tr - y_tr), torch.abs(x_tr - q), torch.abs(y_tr - q)))
         d = self.final_act(self.fc2(self.act(self.fc1(qxy_tr)))).reshape(x.shape[0], y.shape[0])
         return d
 
