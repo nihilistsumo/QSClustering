90cdd5b5ffb8b92aed7e6abb788a277979db2fd2
nihilistsumo <sumantakashyapi@gmail.com>
2021-12-02 00:30:51 -0500

    add dkm param

--- core/clustering.py
+++ core/clustering.py
@@ -77,6 +77,79 @@ class DKM(nn.Module):
         return self.C, self.a
 
 
+class DKM_param(nn.Module):
+
+    def __init__(self, temp=0.05, threshold=0.0001, max_iter=100, eps=1e-6, emb_dim=768, l1_size=512):
+        super(DKM_param, self).__init__()
+        self.temp = temp
+        self.threshold = threshold
+        self.max_iter = max_iter
+        self.emb_dim = emb_dim
+        self.softmax = nn.Softmax(dim=1)
+        self.l1_size = l1_size
+        self.fc1 = nn.Linear(2 * self.emb_dim, self.l1_size)
+        self.fc2 = nn.Linear(self.l1_size, 1)
+        self.act = nn.ReLU()
+        self.final_act = nn.Tanh()
+        self.eps = eps
+
+    def learned_dist(self, x, y):
+        x_tr = x.repeat_interleave(y.shape[0], dim=0)
+        y_tr = y.repeat((x.shape[0], 1))
+        xy_tr = torch.hstack((x_tr, y_tr))
+        d = self.final_act(self.fc2(self.act(self.fc1(xy_tr)))).reshape(x.shape[0], y.shape[0])
+        return d
+
+    def forward(self, X, C_init):
+        self.emb_dim = X.shape[1]
+        self.C = C_init
+        # self.d = -torch.cdist(X, C_init, p=2.0)
+        self.d = -self.learned_dist(X, C_init)
+        # self.a = self.softmax(self.cosine_sim(X, C_init) / self.temp)
+        self.a = self.softmax(self.d / self.temp)
+        self.a_sum = torch.sum(self.a, dim=0) + self.eps
+        self.C_new = torch.matmul(self.a.T, X) / self.a_sum.repeat((self.emb_dim, 1)).T
+        diff = torch.norm(self.C_new - self.C, p=1).item()
+        i = 0
+        while diff > self.threshold and i < self.max_iter:
+            self.C = self.C_new
+            # self.d = -torch.cdist(X, self.C, p=2.0)
+            self.d = -self.learned_dist(X, C_init)
+            # self.a = self.softmax(self.cosine_sim(X, self.C) / self.temp)
+            self.a = self.softmax(self.d / self.temp)
+            self.a_sum = torch.sum(self.a, dim=0) + self.eps
+            self.C_new = torch.matmul(self.a.T, X) / self.a_sum.repeat((self.emb_dim, 1)).T
+            diff = torch.norm(self.C_new - self.C, p=1).item()
+            i += 1
+        return self.C, self.a
+
+
+class QuerySpecificDKM(nn.Module):
+    def __init__(self, sbert_model_name, device):
+        super(QuerySpecificDKM, self).__init__()
+        self.emb_model = SentenceTransformer(sbert_model_name)
+        self.device = device
+        self.dkm = DKM_param(emb_dim=self.emb_model.get_sentence_embedding_dimension())
+        self.dkm.to(device)
+
+    def forward(self, texts, k):
+        n = len(texts)
+        self.emb_vecs = torch.tensor(self.emb_model.encode(texts), device=self.device)
+        init_indices = random.sample(range(n), k)
+        init_c = self.emb_vecs[init_indices]
+        self.C, self.a = self.dkm(self.emb_vecs, init_c)
+        return self.C, self.a
+
+    def get_clustering(self, texts, k):
+        n = len(texts)
+        self.emb_vecs = torch.tensor(self.emb_model.encode(texts), device=self.device)
+        init_indices = random.sample(range(n), k)
+        init_c = self.emb_vecs[init_indices]
+        self.C, self.a = self.dkm(self.emb_vecs, init_c)
+        pred_labels = torch.argmax(self.a, dim=1).detach().cpu().numpy()
+        return pred_labels
+
+
 class QuerySpecificClusteringModel(nn.Module):
     def __init__(self, trans_model_name, emb_dim, device, max_len):
         super(QuerySpecificClusteringModel, self).__init__()
--- experiments/vital_wiki_clustering.py
+++ experiments/vital_wiki_clustering.py
@@ -8,7 +8,8 @@ from torch import Tensor
 from torch.utils.data import DataLoader
 import torch.nn as nn
 from sklearn.metrics import adjusted_rand_score, normalized_mutual_info_score
-from core.clustering import QuerySpecificClusteringModel, SBERTTripletLossModel, get_nmi_loss, get_weighted_adj_rand_loss
+from core.clustering import QuerySpecificClusteringModel, SBERTTripletLossModel, get_nmi_loss, \
+    get_weighted_adj_rand_loss, QuerySpecificDKM
 from util.data import Vital_Wiki_Dataset, get_article_qrels, get_rev_qrels
 from tqdm import tqdm, trange
 import json
@@ -47,6 +48,26 @@ def do_eval(dataset, model, device):
     return rand / num, nmi / num
 
 
+def do_dkm_param_eval(dataset, model):
+    model.eval()
+    rand = 0
+    nmi = 0
+    num = 0
+    preds = []
+    for article_sample in dataset:
+        for batch in article_sample:
+            true_labels = batch.para_labels
+            input_texts = batch.para_texts
+            k = len(set(true_labels))
+            pred_labels = model.get_clustering(input_texts, k)
+            preds.append(pred_labels)
+            rand += adjusted_rand_score(true_labels, pred_labels)
+            nmi += normalized_mutual_info_score(true_labels, pred_labels)
+            num += 1
+    print(random.sample(preds, 1)[0])
+    return rand / num, nmi / num
+
+
 def do_triplet_eval(dataset, model):
     model.eval()
     rand = 0
@@ -64,6 +85,55 @@ def do_triplet_eval(dataset, model):
             num += 1
     print(random.sample(preds, 1)[0])
     return rand / num, nmi / num
+
+
+def treccar_clustering_dkm_param_model(train_dataset,
+                                    test_dataset,
+                                    device,
+                                    val_dataset=None,
+                                    loss_name='nmi',
+                                    max_grad_norm=1.0,
+                                    warmup=10000,
+                                    lrate=2e-5,
+                                    num_epochs=5,
+                                    sbert_model_name='all-mpnet-base-v2'):
+    num_steps_per_epoch = len(train_dataset)
+    num_train_steps = num_epochs * num_steps_per_epoch
+    model = QuerySpecificDKM(sbert_model_name, device)
+    opt = AdamW(model.parameters(), lr=lrate)
+    schd = transformers.get_linear_schedule_with_warmup(opt, warmup, num_epochs * num_train_steps)
+    val_rand, val_nmi = do_dkm_param_eval(val_dataset, model)
+    test_rand, test_nmi = do_dkm_param_eval(test_dataset, model)
+    print('\nVal RAND %.4f, Val NMI %.4f, Test RAND %.4f, Test NMI %.4f' % (val_rand, val_nmi, test_rand, test_nmi))
+    if loss_name == 'nmi':
+        loss_func = get_nmi_loss
+    else:
+        loss_func = get_weighted_adj_rand_loss
+    for epoch in tqdm(range(num_epochs)):
+        train_ids = list(range(len(train_dataset)))
+        random.shuffle(train_ids)
+        for idx in tqdm(range(len(train_ids))):
+            if idx > 0 and idx % 500 == 0:
+                val_rand, val_nmi = do_dkm_param_eval(val_dataset, model)
+                test_rand, test_nmi = do_dkm_param_eval(test_dataset, model)
+                print('\nVal RAND %.4f, Val NMI %.4f, Test RAND %.4f, Test NMI %.4f' % (val_rand, val_nmi, test_rand, test_nmi))
+            model.train()
+            article_sample = train_dataset[train_ids[idx]]
+            for batch in article_sample:
+                k = len(set(batch.para_labels))
+                mc, ma = model(batch.para_texts, k)
+                loss = loss_func(ma, batch.para_labels, device)
+                loss.backward()
+                nn.utils.clip_grad_norm_(model.parameters(), max_grad_norm)
+                opt.step()
+                opt.zero_grad()
+                schd.step()
+        print('Evaluation')
+        print('==========')
+        val_rand, val_nmi = do_dkm_param_eval(val_dataset, model)
+        print('Validation RAND %.4f, NMI %.4f' % (val_rand, val_nmi))
+        test_rand, test_nmi = do_dkm_param_eval(test_dataset, model)
+        print('Test RAND %.4f, NMI %.4f' % (test_rand, test_nmi))
 
 
 def treccar_clustering_single_model(train_dataset,
@@ -244,7 +314,7 @@ parser.add_argument('-vt', '--vital_cats', help='Path to vital article categorie
 parser.add_argument('-aq', '--art_qrels', help='Path to article qrels', default='/home/sk1105/QSC_data/train/base.train.cbor-article.qrels')
 parser.add_argument('-tq', '--qrels', help='Path to toplevel/ hierarchical qrels', default='/home/sk1105/QSC_data/train/base.train.cbor-toplevel.qrels')
 parser.add_argument('-pt', '--para_texts', help='Path to paratext file', default='/home/sk1105/QSC_data/train/train_paratext.tsv')
-parser.add_argument('-ne', '--experiment', type=int, help='Choose the experiment to run (1: QSC/ 2: baseline)', default=1)
+parser.add_argument('-ne', '--experiment', type=int, help='Choose the experiment to run (1: QSC/ 2: DKM param/ 3: baseline)', default=2)
 
 args = parser.parse_args()
 
@@ -261,4 +331,6 @@ test_dataset = Vital_Wiki_Dataset(vital_cats[2], q_paras, rev_para_qrels, args.p
 if args.experiment == 1:
     treccar_clustering_single_model(train_dataset, test_dataset, device, val_dataset)
 elif args.experiment == 2:
+    treccar_clustering_dkm_param_model(train_dataset, test_dataset, device, val_dataset)
+elif args.experiment == 3:
     treccar_clustering_baseline_sbert_triplet_model(train_dataset, test_dataset, device, val_dataset)
