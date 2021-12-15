c8ff454e53d0a388f9ffb8aeaf3dd5d2c79c36d5
nihilistsumo <sumantakashyapi@gmail.com>
2021-12-14 23:25:54 -0500

    treccar 2cv model save add

--- experiments/cv_treccar_clustering.py
+++ experiments/cv_treccar_clustering.py
@@ -49,15 +49,16 @@ def do_eval(test_samples, model, qc=None, triplet_model=False):
 def treccar_clustering_single_model(treccar_2cv_data_file,
                                     device,
                                     loss_name,
-                                    query_context_ref=None,
-                                    max_num_tokens=128,
-                                    max_grad_norm=1.0,
-                                    weight_decay=0.01,
-                                    warmup=1000,
-                                    lrate=2e-5,
-                                    num_epochs=75,
-                                    emb_model_name='sentence-transformers/all-MiniLM-L6-v2',
-                                    emb_dim=256):
+                                    query_context_ref,
+                                    max_num_tokens,
+                                    max_grad_norm,
+                                    weight_decay,
+                                    warmup,
+                                    lrate,
+                                    num_epochs,
+                                    emb_model_name,
+                                    emb_dim,
+                                    output_path):
     if query_context_ref is not None:
         with open(query_context_ref, 'r') as f:
             qc = json.load(f)
@@ -121,6 +122,11 @@ def treccar_clustering_single_model(treccar_2cv_data_file,
                                                                                                 np.std(list(test_rand.values()), ddof=1) / np.sqrt(len(test_rand.keys())),
                                                                                                 np.mean(list(test_nmi.values())),
                                                                                                 np.std(list(test_nmi.values()), ddof=1) / np.sqrt(len(test_nmi.keys()))))
+        if output_path is not None:
+            print('Saving the trained model...')
+            torch.save(model.state_dict(), output_path+'_fold'+str(i+1)+'.model')
+            model = QuerySpecificClusteringModel(emb_model_name, emb_dim, device, max_num_tokens)
+            model.load_state_dict(torch.load(output_path+'_fold'+str(i+1)+'.model'))
         print('Evaluation Fold %d' % (i+1))
         print('=================')
         if query_context_ref is not None:
@@ -135,15 +141,16 @@ def treccar_clustering_single_model(treccar_2cv_data_file,
 
 def treccar_clustering_baseline_sbert_triplet_model(treccar_2cv_data_file,
                                     device,
-                                    max_num_tokens=128,
-                                    max_grad_norm=1.0,
-                                    weight_decay=0.01,
-                                    warmup=1000,
-                                    lrate=2e-5,
-                                    num_epochs=75,
-                                    emb_model_name='sentence-transformers/all-MiniLM-L6-v2',
+                                    max_num_tokens,
+                                    max_grad_norm,
+                                    weight_decay,
+                                    warmup,
+                                    lrate,
+                                    num_epochs,
+                                    emb_model_name,
+                                    emb_dim,
+                                    output_path,
                                     triplet_margin=5,
-                                    emb_dim=256,
                                     batch_size=32):
     cv_datasets = np.load(treccar_2cv_data_file, allow_pickle=True)[()]['data']
     for i in range(len(cv_datasets)):
@@ -199,6 +206,11 @@ def treccar_clustering_baseline_sbert_triplet_model(treccar_2cv_data_file,
                                                                                                 np.std(list(test_rand.values()), ddof=1) / np.sqrt(len(test_rand.keys())),
                                                                                                 np.mean(list(test_nmi.values())),
                                                                                                 np.std(list(test_nmi.values()), ddof=1) / np.sqrt(len(test_nmi.keys()))))
+        if output_path is not None:
+            print('Saving the trained model...')
+            torch.save(model.state_dict(), output_path+'_fold'+str(i+1)+'.model')
+            model = QuerySpecificClusteringModel(emb_model_name, emb_dim, device, max_num_tokens)
+            model.load_state_dict(torch.load(output_path+'_fold'+str(i+1)+'.model'))
         print('Evaluation Fold %d' % (i+1))
         print('=================')
         test_rand, test_nmi = do_eval(test_data_current, model, triplet_model=True)
@@ -232,14 +244,36 @@ if torch.cuda.is_available():
 else:
     device = torch.device('cpu')
     print('CUDA not available, using device: '+str(device))
+    max_num_tokens = 128,
+    max_grad_norm = 1.0,
+    weight_decay = 0.01,
+    warmup = 1000,
+    lrate = 2e-5,
+    num_epochs = 75,
+    emb_model_name = 'sentence-transformers/all-MiniLM-L6-v2',
+    emb_dim = 256
 parser = argparse.ArgumentParser(description='Run query specific clustering experiments on TRECCAR 2-fold cv dataset')
 parser.add_argument('-vd', '--vital_data', help='Path to TRECCAR clustering npy file prepared for 2-fold cv',
                     default='D:\\new_cats_data\\QSC_data\\benchmarkY1-train-nodup\\treccar_clustering_data_by1train_2cv.npy')
+parser.add_argument('-op', '--output_path', help='Path to save the trained model', default=None)
 parser.add_argument('-ne', '--experiment', type=int, help='Choose the experiment to run (1: QSC/ 2: baseline)', default=1)
 parser.add_argument('-ls', '--loss', help='Loss func to use for QSC', default='adj')
+parser.add_argument('-qc', '--query_con', help='Path to query-context json file', default=None)
+parser.add_argument('-mn', '--model_name', help='SBERT embedding model name', default='sentence-transformers/all-MiniLM-L6-v2')
+parser.add_argument('-nt', '--max_num_tokens', help='Max no. of tokens', default=128)
+parser.add_argument('-gn', '--max_grad_norm', help='Max gradient norm', default=1.0)
+parser.add_argument('-wd', '--weight_decay', default=0.01)
+parser.add_argument('-wu', '--warmup', default=1000)
+parser.add_argument('-lr', '--lrate', default=2e-5)
+parser.add_argument('-ep', '--epochs', default=75)
+parser.add_argument('-ed', '--emb_dim', default=256)
 
 args = parser.parse_args()
 if args.experiment == 1:
-    treccar_clustering_single_model(args.vital_data, device, args.loss)
+    treccar_clustering_single_model(args.vital_data, device, args.loss, args.query_con, args.max_num_tokens,
+                                    args.max_grad_norm, args.weight_decay, args.warmup, args.lrate, args.epochs,
+                                    args.model_name, args.emb_dim, args.output_path)
 elif args.experiment == 2:
-    treccar_clustering_baseline_sbert_triplet_model(args.vital_data, device)
+    treccar_clustering_baseline_sbert_triplet_model(args.vital_data, device, args.max_num_tokens, args.max_grad_norm,
+                                                    args.weight_decay, args.warmup, args.lrate, args.epochs,
+                                                    args.model_name, args.emb_dim, args.output_path)
