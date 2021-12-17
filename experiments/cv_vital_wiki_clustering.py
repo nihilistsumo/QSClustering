7bbaa22384a19420767fae245191666bcb7304f9
nihilistsumo <sumantakashyapi@gmail.com>
2021-12-17 13:30:11 -0500

    add section based loss for vital wiki

--- experiments/cv_vital_wiki_clustering.py
+++ experiments/cv_vital_wiki_clustering.py
@@ -9,7 +9,7 @@ from torch.utils.data import DataLoader
 import torch.nn as nn
 from sklearn.metrics import adjusted_rand_score, normalized_mutual_info_score
 from core.clustering import QuerySpecificClusteringModel, SBERTTripletLossModel, get_nmi_loss, \
-    get_weighted_adj_rand_loss, QuerySpecificDKM
+    get_weighted_adj_rand_loss, QuerySpecificDKM, QuerySpecificClusteringModelWithSection
 from tqdm import tqdm, trange
 import json
 import numpy as np
@@ -35,7 +35,7 @@ def do_eval(test_samples, model, qc=None, triplet_model=False):
             embeddings = model.emb_model.encode(input_texts, convert_to_tensor=True)
         else:
             texts = s.para_texts
-            query_content = s.category +'. ' + s.q.split('enwiki:')[1].replace('%20', ' ')
+            query_content = s.category
             input_texts = [(query_content, t) for t in texts]
             embeddings = model.qp_model.encode(input_texts, convert_to_tensor=True)
         pred_labels = model.get_clustering(embeddings, k)
@@ -53,7 +53,7 @@ def do_eval_dkm_param(test_samples, model, qc=None):
         true_labels = s.para_labels
         k = len(set(true_labels))
         texts = s.para_texts
-        query_content = s.category +'. ' + s.q.split('enwiki:')[1].replace('%20', ' ')
+        query_content = s.category
         pred_labels = model.get_clustering(query_content, texts, k)
         rand = adjusted_rand_score(true_labels, pred_labels)
         nmi = normalized_mutual_info_score(true_labels, pred_labels)
@@ -119,6 +119,94 @@ def vital_wiki_clustering_single_model(vital_wiki_2cv_data_file,
                 #print(GPUtil.showUtilization())
                 mc, ma = model(input_features, k)
                 loss = loss_func(ma, sample.para_labels, device)
+                loss.backward()
+                #print(batch.q + ' %d paras, Loss %.4f' % (len(batch.paras), loss.detach().item()))
+                nn.utils.clip_grad_norm_(model.parameters(), max_grad_norm)
+                opt.step()
+                opt.zero_grad()
+                schd.step()
+        print('Evaluation Fold %d' % (i+1))
+        print('=================')
+        if query_context_ref is not None:
+            test_rand, test_nmi = do_eval(test_data_current, model, qc)
+        else:
+            test_rand, test_nmi = do_eval(test_data_current, model)
+        print('Mean RAND %.4f +- %.4f, NMI %.4f +- %.4f' % (np.mean(list(test_rand.values())),
+                                                            np.std(list(test_rand.values()), ddof=1) / np.sqrt(len(test_rand.keys())),
+                                                            np.mean(list(test_nmi.values())),
+                                                            np.std(list(test_nmi.values()), ddof=1) / np.sqrt(len(test_nmi.keys()))))
+
+
+def vital_wiki_clustering_single_model_with_sections(vital_wiki_2cv_data_file,
+                                    device,
+                                    clustering_loss_name,
+                                    query_context_ref=None,
+                                    max_num_tokens=128,
+                                    max_grad_norm=1.0,
+                                    weight_decay=0.01,
+                                    warmup=1000,
+                                    lrate=2e-5,
+                                    num_epochs=2,
+                                    emb_model_name='sentence-transformers/all-MiniLM-L6-v2',
+                                    emb_dim=256):
+    if query_context_ref is not None:
+        with open(query_context_ref, 'r') as f:
+            qc = json.load(f)
+    cv_datasets = np.load(vital_wiki_2cv_data_file, allow_pickle=True)[()]['data']
+    alpha = 0.005
+    beta = 0.005
+    for i in range(len(cv_datasets)):
+        train_data_current = cv_datasets[i]
+        test_data_current = train_data_current.test_samples
+        num_steps_per_epoch = len(train_data_current)
+        num_train_steps = num_epochs * num_steps_per_epoch
+        model = QuerySpecificClusteringModelWithSection(emb_model_name, emb_dim, device, max_num_tokens)
+        model_params = list(model.named_parameters())
+        no_decay = ['bias', 'LayerNorm.bias', 'LayerNorm.weight']
+        optimizer_grouped_parameters = [
+            {'params': [p for n, p in model_params if not any(nd in n for nd in no_decay)],
+            'weight_decay': weight_decay},
+            {'params': [p for n, p in model_params if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
+        ]
+        opt = AdamW(optimizer_grouped_parameters, lr=lrate)
+        schd = transformers.get_linear_schedule_with_warmup(opt, warmup, num_epochs * num_train_steps)
+        if query_context_ref is not None:
+            test_rand, test_nmi = do_eval(test_data_current, model, qc)
+        else:
+            test_rand, test_nmi = do_eval(test_data_current, model)
+        print('\nFold %d Initial Test evaluation' % (i+1))
+        print('Mean RAND %.4f +- %.4f, NMI %.4f +- %.4f' % (np.mean(list(test_rand.values())),
+                                                            np.std(list(test_rand.values()), ddof=1) / np.sqrt(len(test_rand.keys())),
+                                                            np.mean(list(test_nmi.values())),
+                                                            np.std(list(test_nmi.values()), ddof=1) / np.sqrt(len(test_nmi.keys()))))
+        if clustering_loss_name == 'nmi':
+            cl_loss_func = get_nmi_loss
+        else:
+            cl_loss_func = get_weighted_adj_rand_loss
+        for epoch in tqdm(range(num_epochs)):
+            for idx in tqdm(range(len(train_data_current))):
+                model.train()
+                sample = train_data_current[idx]
+                query_content = sample.category
+                n = len(sample.paras)
+                k = len(set(sample.para_labels))
+                input_texts = [(query_content, t) for t in sample.para_texts]
+                input_features = model.qp_model.tokenize(input_texts)
+                put_features_in_device(input_features, device)
+                section_texts = []
+                for s in sample.para_labels:
+                    if '/' in s:
+                        section_texts.append((query_content, s.split('/')[1].replace('%20', ' ')))
+                    else:
+                        section_texts.append((query_content, query_content))
+                section_features = model.qp_model.tokenize(section_texts)
+                put_features_in_device(section_features, device)
+                #print(GPUtil.showUtilization())
+                mc, ma, qp_emb, qs_emb = model(input_features, section_features, k)
+                clustering_loss = cl_loss_func(ma, sample.para_labels, device)
+                classification_loss = torch.sum((qp_emb - qs_emb).pow(2).sum(1).sqrt())
+                section_emb_reg = torch.sum(torch.cdist(qs_emb, qs_emb, p=2.0)) / k ** 2
+                loss = clustering_loss + alpha * classification_loss - beta * section_emb_reg
                 loss.backward()
                 #print(batch.q + ' %d paras, Loss %.4f' % (len(batch.paras), loss.detach().item()))
                 nn.utils.clip_grad_norm_(model.parameters(), max_grad_norm)
@@ -291,22 +379,29 @@ def get_triplet_texts_from_sample(sample):
     return input_texts
 
 
-if torch.cuda.is_available():
-    device = torch.device('cuda')
-    print('CUDA is available and using device: '+str(device))
-else:
-    device = torch.device('cpu')
-    print('CUDA not available, using device: '+str(device))
-parser = argparse.ArgumentParser(description='Run query specific clustering experiments on Vital wiki 2-fold cv dataset')
-parser.add_argument('-vd', '--vital_data', help='Path to vital wiki clustering npy file prepared for 2-fold cv',
-                    default='D:\\new_cats_data\\QSC_data\\vital_wiki\\vital_wiki_clustering_data_2cv.npy')
-parser.add_argument('-ne', '--experiment', type=int, help='Choose the experiment to run (1: QSC/ 2: DKM param/ 3: baseline)', default=2)
-parser.add_argument('-ls', '--loss', help='Loss func to use for QSC', default='adj')
+def main():
+    if torch.cuda.is_available():
+        device = torch.device('cuda')
+        print('CUDA is available and using device: '+str(device))
+    else:
+        device = torch.device('cpu')
+        print('CUDA not available, using device: '+str(device))
+    parser = argparse.ArgumentParser(description='Run query specific clustering experiments on Vital wiki 2-fold cv dataset')
+    parser.add_argument('-vd', '--vital_data', help='Path to vital wiki clustering npy file prepared for 2-fold cv',
+                        default='D:\\new_cats_data\\QSC_data\\vital_wiki\\vital_wiki_clustering_data_2cv.npy')
+    parser.add_argument('-ne', '--experiment', type=int, help='Choose the experiment to run (1: QSC/ 2: QSC section/ 3: DKM param/ 4: baseline)', default=2)
+    parser.add_argument('-ls', '--loss', help='Loss func to use for QSC', default='adj')
 
-args = parser.parse_args()
-if args.experiment == 1:
-    vital_wiki_clustering_single_model(args.vital_data, device, args.loss)
-elif args.experiment == 2:
-    vital_wiki_clustering_dkm_param_model(args.vital_data, device, args.loss)
-elif args.experiment == 3:
-    vital_wiki_clustering_baseline_sbert_triplet_model(args.vital_data, device)
+    args = parser.parse_args()
+    if args.experiment == 1:
+        vital_wiki_clustering_single_model(args.vital_data, device, args.loss)
+    elif args.experiment == 2:
+        vital_wiki_clustering_single_model_with_sections(args.vital_data, device, args.loss)
+    elif args.experiment == 3:
+        vital_wiki_clustering_dkm_param_model(args.vital_data, device, args.loss)
+    elif args.experiment == 4:
+        vital_wiki_clustering_baseline_sbert_triplet_model(args.vital_data, device)
+
+
+if __name__ == '__main__':
+    main()
